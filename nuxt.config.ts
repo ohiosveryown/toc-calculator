418ec9639c7a0964e38286f02c42953cd5013df1
ohiosveryown <matt@ohiosveryown.co>
2026-02-04 14:47:13 -0500

    added type scale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/style/reset.scss'],
+  css: ['~/style/reset.scss', '~/style/type.scss'],
 })
